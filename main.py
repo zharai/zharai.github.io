53fe05f99b3e2ae69f7f72b832167dc4723cfc80
tanvia09 <136640828+tanvia09@users.noreply.github.com>
2024-10-31 22:32:52 -0400

    Added a base html form, progress towards fetching and displaying info

--- main.py
+++ main.py
@@ -1,6 +1,29 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, redirect, url_for, request
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
-@app.route("/homepage")
+app = Flask(__name__, template_folder="templates")
+
+@app.route("/home")
 def home():
-    return
+    return render_template("homepage.html")
+
+@app.route("/applications")
+def test():
+    return render_template("applicationPage.html")
+
+@app.route("/postJobs", methods=["POST", "GET"])
+def jobPostingPage():
+    if request.method == "POST": 
+        company_name = request.form ["company_name"]
+        return redirect(url_for('display', display=company_name)) 
+    else:
+        return render_template("postJobs.html")
+
+@app.route("/<display>")
+def display(display):
+    return render_template("displayPage.html", display=display)
+
+if __name__ == "__main__":
+    app.run(debug=True)
