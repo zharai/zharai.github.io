f6146e909cdf6384d45eca61f843aa6da494010b
tanvia09 <136640828+tanvia09@users.noreply.github.com>
2024-11-07 18:13:38 -0500

    Fixed submit form (no database)

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
+import sqlalchemy 
 
 
 app = Flask(__name__, template_folder="templates")
@@ -21,11 +22,11 @@ def jobPostingPage():
         company_name = request.form ["company_name"]
         job_title = request.form ["job_title"]
         description = request.form["description"]
-        return redirect(url_for('display', display=company_name + " is looking for a " + job_title + ". Details:(" + description + ")")) 
+        return redirect(url_for("display", display=company_name + " is looking for a " + job_title + ". Details:(" + description + ")")) 
     else:
         return render_template("postJobs.html")
 
-@app.route("/display/<display>")
+@app.route("/<display>")
 def display(display):
     return render_template("displayPage.html", display=display)
 
