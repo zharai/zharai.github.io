505b43b1de2039150c7e3900ba897748f36b3454
tanvia09 <136640828+tanvia09@users.noreply.github.com>
2024-11-07 22:00:23 -0500

    Created session

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 from flask_wtf import FlaskForm, CSRFProtect
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired, Length
-import sqlalchemy 
+from flask_sqlalchemy import SQLAlchemy
+from datetime import timedelta
 
 
 app = Flask(__name__, template_folder="templates")
+app.secret_key = "hi3u4h"
+app.permanent_session_lifetime = timedelta(days=30)
 
 @app.route("/home")
 def home():
@@ -17,18 +20,25 @@ def applications():
     return render_template("applicationPage.html")
 
 @app.route("/postJobs", methods=["POST", "GET"])
-def jobPostingPage():
+def postJobs():
     if request.method == "POST": 
-        company_name = request.form ["company_name"]
-        job_title = request.form ["job_title"]
-        description = request.form["description"]
-        return redirect(url_for("display", display=company_name + " is looking for a " + job_title + ". Details:(" + description + ")")) 
+        session.permanent = True
+        display = request.form["company_name"]
+
+        session["display"] = display
+
+        return redirect(url_for("display")) 
     else:
         return render_template("postJobs.html")
 
-@app.route("/<display>")
-def display(display):
-    return render_template("displayPage.html", display=display)
+@app.route("/display")
+def display():
+    if "display" in session:
+        display = session["display"]
+        return f"<h1>{display}</h1>"
+    else:
+        return redirect(url_for("postJobs"))
 
 if __name__ == "__main__":
+
     app.run(debug=True)
