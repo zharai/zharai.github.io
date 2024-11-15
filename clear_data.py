1f8ef913e2658a1b945b7ee268e728dc220023f6
zharai <94867188+zharai@users.noreply.github.com>
2024-11-15 00:07:13 -0500

    fixed clear_data file, also added style.css

--- clear_data.py
+++ clear_data.py
@@ -1,9 +1,21 @@
-from main import app, db, Info, Application 
+from main import app, db  # Import app and db directly if create_app does not exist
+from main import Employer, Info, Application  # Import your models
 
+def clear_all_data():
+    try:
+        # Delete data from each table
+        db.session.query(Employer).delete()
+        db.session.query(Info).delete()
+        db.session.query(Application).delete()
+        
+        # Commit the changes to the database
+        db.session.commit()
+        print("Data cleared from all tables successfully.")
+
+    except Exception as e:
+        db.session.rollback()  # Rollback if there is an error
+        print("An error occurred:", e)
+
+# Use the application context
 with app.app_context():
-    # Clear all data in the tables
-    db.session.query(Info).delete()
-    db.session.query(Application).delete()
-    db.session.commit()
-
-    print("Data cleared from Info and Application tables!")
+    clear_all_data()
